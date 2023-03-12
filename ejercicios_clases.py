d02532cb1be3c2bc893882e32f6535294b328e12
ChristianDP76 <christdp76@gmail.com>
2023-03-12 16:51:12 -0500

    Practica -ClaseUsuario

--- ejercicios_clases.py
+++ ejercicios_clases.py
@@ -1,32 +1,73 @@
+from pprint import pprint
 # Crear una Clase Operaciones con sus respectivos métodos
 # METODOS: Sumar - Restar - Multiplicar - Dividir
 # Esta clase recibirá 2 parámetros
 
-class OperacionesMatematicas:
-    def __init__(self, valor1, valor2):
-        self.a = valor1
-        self.b = valor2
+# class OperacionesMatematicas:
+#    def __init__(self, valor1, valor2):
+ #       self.a = valor1
+ #       self.b = valor2
 
-    def Suma(self):
-        return self.a + self.b
+ #   def Suma(self):
+ #       return self.a + self.b
     
-    def Resta(self):
-        return self.a - self.b
+ #   def Resta(self):
+ #        return self.a - self.b
     
-    def Multiplicacion(self):
-        return self.a * self.b
+  #  def Multiplicacion(self):
+ #       return self.a * self.b
     
-    def Dividir(self):
-        return self.__redondear(self.a / self.b)
+  #  def Dividir(self):
+ #       return self.__redondear(self.a / self.b)
     
     # Método Privado '__Redondear' 
-    def __redondear(self, numero):
-        return round(numero, 2)
+  #  def __redondear(self, numero):
+ #       return round(numero, 2)
         
 
-Opera = OperacionesMatematicas(5, 4)
+# Opera = OperacionesMatematicas(5, 4)
 
-print(Opera.Dividir())
+# print(Opera.Dividir())
+# --------------------------------------------------------------
+# Crear una Clase Usuario que reciba los datos de un usuario
+# Nombre - Edad - DNI - Estatura - EstadoCivil
+# Convertir estos datos en un Diccionario
+
+class Usuario:
+    def __init__(self, Nombre, Edad, Dni, Estatura, ECivil) -> None:
+        self.nombre= Nombre
+        self.edad = Edad
+        self.dni = Dni
+        self.estatura = Estatura
+        self.estadoCivil = ECivil
+
+    def convertirDiccionario(self):
+        return{
+            'Nombre': self.nombre,
+            'Edad': self.edad,
+            'DNI': self.dni,
+            'Estatura': self.estatura,
+            'EstadoCivil': self.estadoCivil
+        }
+
+usuario = Usuario('Christian', 46, 29700824, 1.70, 'Casado')
+pprint(usuario.convertirDiccionario())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     
