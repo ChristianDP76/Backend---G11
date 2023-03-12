b8eedf2a9fa310f0e41edac81efc9b37a9803678
ChristianDP76 <christdp76@gmail.com>
2023-03-12 13:59:29 -0500

    metodo __str__ y self

--- clases.py
+++ clases.py
@@ -1,5 +1,5 @@
 # con 'self' nos permite acceder al contenido de una CLASE.
-# 
+# metodos magicos '__init__' permite extraer o recibir los valores que le estamos dando a nuestra clase
 
 class Vehiculo:
     def __init__(self, color, placa, marca) -> None:
@@ -15,9 +15,28 @@ class Vehiculo:
 
 vehiculo = Vehiculo("rojo","V21-543", "Honda")
 
-print(vehiculo.verificarEstado(1999))
-print(vehiculo.concatenarCaracteristicas())
+#print(vehiculo.verificarEstado(1999))
+#print(vehiculo.concatenarCaracteristicas())
 
+# -- Usando el método '__str__' nos retorna un valor a esa clase
+class Alumno:
+    def __init__(self, nombre, edad) -> None:
+        self.nombre = nombre
+        self.edad = edad
+
+    def __str__(self) -> str:
+        return f'Nombre: {self.nombre}, Edad: {self.edad}'
+    
+    def mostrarNombre(self):
+        return self.nombre
+    
+    def mostrarEdad(self):
+        return self.edad
+
+
+x = Alumno('Christian', 30)
+
+print(x.mostrarNombre())
 
 
     
